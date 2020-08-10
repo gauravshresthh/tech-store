08eee307f377d6332d3f8edf9bafaca20749f21f
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-10 11:22:25 +0545

    basic routing

--- src/App.js
+++ src/App.js
@@ -10,8 +10,23 @@ import SingleProduct from './pages/SingleProductPage';
 import Cart from './pages/CartPage';
 import Default from './pages/Default';
 
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
-  return <div className="App">Hello pika</div>;
+  return (
+    <>
+      {/* navbar,sidebar,cart,footer */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id" exact component={SingleProduct} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
