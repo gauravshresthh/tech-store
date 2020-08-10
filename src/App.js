fe6ed67655af9b2730215cedb231ec669261f999
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-10 11:13:29 +0545

    folder structured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
-
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './pages/HomePage';
+import Contact from './pages/ContactPage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import SingleProduct from './pages/SingleProductPage';
+import Cart from './pages/CartPage';
+import Default from './pages/Default';
 
 function App() {
   return <div className="App">Hello pika</div>;
